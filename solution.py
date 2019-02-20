7b3ff642f1dc66a253b3c733f486b1902bd764d1
Tim Fielder <fiel1139@gmail.com>
2019-02-20 15:50:32 -0700

    definitions for functions.

--- solution.py
+++ solution.py
@@ -11,23 +11,72 @@
          # ]
 
 class Solution:
-    m = 0
-    n = 0
-    b = []
 
-    def set_board(board):
-        b = board
-        set_board_size()
+    # def set_board_size(self,board):
+    #     m = len(board)
+    #     n = len(board[0])
 
-    def set_board_size(board):
-        m = len(board)
-        n = len(board[0])
+    def set_board(self, board):
+      print("Made it!")
+      #b = board
+      #set_board_size(self, board)
 
-    def return_board():
-        return b
+    def return_board(self):
+      return b
+
+    # def return_columns(self):
+    #     return m
+
+    # def return_rows(self):
+    #     return n
 
     def run(self, board):
-        set_board(board)
+        def set_board(self, board):
+          self.b = board
+          self.m = len(board)
+          self.n = len(board[0])
+
+        def return_board(self):
+          return self.b
+
+        def return_rows(self):
+          return self.m
+
+        def return_columns(self):
+          return self.n
+
+        def create_new_board(self):
+          self.nb = ([[0] * self.n]) * self.m
+
+        def return_new_board(self):
+          return self.nb
+
+        def find_neighbors(self, coordinates):
+          count = 0
+          for i in range(-1, 2):
+            for j in range(-1, 2):
+              if coordinates[0 + i] and coordinates[1+ j]
+                print(board[coordinates[0]+i]][coordinates[1]+j]])
+
+        def get_coordinates(self):
+          for i in range(return_rows(self)):
+            for j in range(return_columns(self)):
+              coordinates = [i, j]
+              find_neighbors(self, coordinates)
+
+        def update_new_board(self):
+          return self.nb
+
+        set_board(self, board)
+        create_new_board(self)
+        get_coordinates(self)
+        return_new_board(self)
+        return_board(self)
+
+b1 = [[1,0,1],[1,1,1],[0,1,0],[0,0,1]]
+
+s1 = Solution()
+s1.run(b1)
 
     #determine size of board
     #iterate through each cell
