ea2782e706b1174b645e15015072956bcb78ef1b
Tim Fielder <fiel1139@gmail.com>
2019-02-20 16:56:57 -0700

    Updates the new board.

--- solution.py
+++ solution.py
@@ -12,24 +12,6 @@
 
 class Solution:
 
-    # def set_board_size(self,board):
-    #     m = len(board)
-    #     n = len(board[0])
-
-    def set_board(self, board):
-      print("Made it!")
-      #b = board
-      #set_board_size(self, board)
-
-    def return_board(self):
-      return b
-
-    # def return_columns(self):
-    #     return m
-
-    # def return_rows(self):
-    #     return n
-
     def run(self, board):
         def set_board(self, board):
           self.b = board
@@ -48,8 +30,17 @@ class Solution:
         def create_new_board(self):
           self.nb = ([[0] * self.n]) * self.m
 
+        def update_new_board(self, coordinates, amount):
+          self.nb[coordinates[0]][coordinates[1]] = amount
+
         def return_new_board(self):
           return self.nb
+
+        def within_range(self, coordinate, elements):
+          if coordinate > 0 and coordinate < elements:
+            return True
+          else:
+            return False
 
         def find_neighbors(self, coordinates):
           count = 0
@@ -57,24 +48,34 @@ class Solution:
             for j in range(-1, 2):
               first = coordinates[0] + i
               second = coordinates[1] + j
-
-              if self.b[first][second]
-              # print(cool)
+              if (within_range(self, first, self.m)) and (within_range(self, second, self.n)):
+                if i == 0 and j == 0:
+                  continue
+                else:
+                  count += self.b[coordinates[0] + i][coordinates[1] + j]
+          return count
 
         def get_coordinates(self):
           for i in range(return_rows(self)):
             for j in range(return_columns(self)):
               coordinates = [i, j]
               find_neighbors(self, coordinates)
+              amt = find_neighbors(self, coordinates)
+              print(amt)
+              update_new_board(self, coordinates, amt)
+
+
+
 
         def update_new_board(self):
           return self.nb
 
         set_board(self, board)
         create_new_board(self)
-        get_coordinates(self)
         return_new_board(self)
-        return_board(self)
+        get_coordinates(self)
+
+        #return_board(self)
 
 
 
@@ -82,6 +83,8 @@ b1 = [[1,0,1],[1,1,1],[0,1,0],[0,0,1]]
 
 s1 = Solution()
 s1.run(b1)
+
+    
 
     #determine size of board
     #iterate through each cell
