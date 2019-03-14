dfc394a2dae1de80298c2d750b7de2c015e1b5be
Tim Fielder <fiel1139@gmail.com>
2019-03-13 22:58:20 -0600

    Adds an introductory message.

--- solution.py
+++ solution.py
@@ -1,37 +1,42 @@
 class Solution():
+  def run(self, board):
+    response = "r"
+    while response != "q":
+      self.set_board(board)
+      self.welcome_message()
+      self.create_new_board()
+      self.check_for_neighbors()
+      self.update_board()
+      self.print_board()
+      x = input()
+
+  def welcome_message(self):
+    print("Welcome to Conway's Game of Life")
+    print("1s represent living cells, zeros represent non-living cells")
+    print("Your board starts like this:")
+    self.print_board()
+    print("Press the spacebar to iterate through each moment in time.")
+    print("Type 'q' to quit at any point.")
+
   def set_board(self, board):
     self.board = board
     self.rows_count = len(board)
     self.columns_count = len(board[0])
 
-  def print_board(self):
-    print('')
-    for row in range(self.rows_count):
-        print(self.board[row])
-    print('')
-
   def create_new_board(self):
     self.newboard = ([[0] * self.columns_count]) * self.rows_count
 
-  def update_new_board(self, coordinates, amount):
-    self.newboard[coordinates[0]][coordinates[1]] = amount
-
-  def return_new_board(self):
-    return self.newboard
-
-  def within_range(self, coordinate, elements):
-    if coordinate >= 0 and coordinate < elements:
-      return True
-    else:
-      return False
-
-  def get_neighbors(self):
+  def check_for_neighbors(self):
     for row in range(self.rows_count):
       for column in range(self.columns_count):
         coordinates = [row, column]
-        self.find_neighbors(coordinates)
+        neighbors =self.find_neighbors_at_coordinate(coordinates)
+        self.update_new_board(coordinates, neighbors)
 
-  def find_neighbors(self, coordinates):
+  def update_new_board(self, coordinates, neighbors):
+    self.newboard[coordinates[0]][coordinates[1]] = neighbors
+
+  def find_neighbors_at_coordinate(self, coordinates):
     neighbors = 0
     for row in range(-1 , 2):
       for column in range(-1, 2):
@@ -42,8 +47,13 @@ class Solution():
             continue
           else:
             neighbors += self.board[coordinates[0] + row][coordinates[1] + column]
-    self.newboard[coordinates[0]][coordinates[1]] = neighbors
     return neighbors
+
+  def within_range(self, coordinate, elements):
+    if coordinate >= 0 and coordinate < elements:
+      return True
+    else:
+      return False
 
   def update_board(self):
     for row in range(self.rows_count):
@@ -54,12 +64,11 @@ class Solution():
         if self.board[row][column] == 0 and self.newboard[row][column] == 3:
             self.board[row][column] = 1
 
-  def run(self, board):
-    self.set_board(board)
-    self.create_new_board()
-    self.get_neighbors()
-    self.update_board()
-    self.print_board()
+  def print_board(self):
+    print('')
+    for row in range(self.rows_count):
+        print(self.board[row])
+    print('')
 
 def main():
   b1 = [[1,0,1],
@@ -70,10 +79,10 @@ def main():
         [1,1,1],
         [0,1,1]]
 
-  b3 = [[0,0,0],
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]]
+  b3 = [[0,1,0],
+        [0,1,1],
+        [1,0,1],
+        [1,1,0]]
 
   s1 = Solution()
   s1.run(b3)
